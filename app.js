dd31593e08d024e159e8b34e9db039365f63a328
maxjacobson <max.jacobson@torchlms.com>
2019-03-18 15:49:17 -0600

    If no dependency, put null instead of empty string

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ class Installer {
             // console.log(item.split(':')[0]) //item name
             // console.log(item.split(':')[1].trim()) //item dependency
 
-            this.packages[item.split(':')[0]] = new Package(item.split(':')[0], item.split(':')[1].trim());
+            this.packages[item.split(':')[0]] = new Package(item.split(':')[0], item.split(':')[1].trim() == '' ? null : item.split(':')[1].trim());
         })
 
-        console.log(this.packages);
+        for(let item in this.packages) {
+            
+        }
     }
 }
 
